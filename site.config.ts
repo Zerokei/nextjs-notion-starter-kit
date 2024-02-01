2507294d94462abc4cddc6d5366ff513ff18d2c5
Chritch | 克瑞奇 <Yezk@zju.edu.cn>
2024-02-01 23:20:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8b92b77cce4e4545955a3f694d1a0e27',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Restore',
+  domain: 'restoredsouls.com',
+  author: 'Christians',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Jesus restores our souls.',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
